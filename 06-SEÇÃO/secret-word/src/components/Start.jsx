a0f0539ffb557d8a1dc49f73811027fc221b3a02
andrewDeras <andrewderas7@gmail.com>
2023-01-16 22:22:01 -0300

    Secret Word

---
+++ 06-SEÇÃO/secret-word/src/components/Start.jsx
@@ -0,0 +1,13 @@
+import './Start.css'
+
+const Start = () => {
+  return (
+    <div className='start' >
+      <h1>Secret Word</h1>
+      <p>Clique no botão para começar a jogar!!</p>
+      <button>Começar o jogo</button>
+    </div>
+  )
+}
+
+export default Start
