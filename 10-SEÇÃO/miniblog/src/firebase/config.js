9fb3a106c33476d646ffc4322a6bb81ff861a3c1
andrewDeras <andrewderas7@gmail.com>
2023-03-05 16:56:20 -0300

    keep pushing

---
+++ 10-SEÇÃO/miniblog/src/firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCmkKjQHpsOJfilJjeCqDoM3tWHEVInAn0",
+  authDomain: "mini-blog-ea386.firebaseapp.com",
+  projectId: "mini-blog-ea386",
+  storageBucket: "mini-blog-ea386.appspot.com",
+  messagingSenderId: "708419790729",
+  appId: "1:708419790729:web:20d17087033b13c4a1e358"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
