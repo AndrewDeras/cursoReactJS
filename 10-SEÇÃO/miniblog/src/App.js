cd02330c0e064f49b951d99bd477526e58a6a58b
andrewDeras <andrewderas7@gmail.com>
2023-03-11 17:53:06 -0300

    implementando login e logout

--- 10-SEÇÃO/miniblog/src/App.js
+++ 10-SEÇÃO/miniblog/src/App.js
@@ -39,7 +39,7 @@ function App() {
 
   return (
     <div className="App">
-      <AuthProvider value={user} >
+      <AuthProvider value={{user}} >
         <BrowserRouter>
           <Navbar />
           <div className="container">
--- 10-SEÇÃO/miniblog/src/hooks/useAuthentication.jsx
+++ 10-SEÇÃO/miniblog/src/hooks/useAuthentication.jsx
@@ -50,14 +50,49 @@ export const useAuthentication = () => {
       let systemErrorMessage
 
       if (error.message.includes('Password')) {
-        systemErrorMessage = 'A senha precisa conter pelo menos 6 caracteres.'
+        systemErrorMessage = 'A senha precisa conter pelo menos 6 caracteres.';
       } else if (error.message.includes('email-already')) {
-        systemErrorMessage = 'Email já cadastrado.'
+        systemErrorMessage = 'Email já cadastrado.';
       } else {
-        systemErrorMessage = 'Ocorreu um erro, por favor tente mais tarde.'
+        systemErrorMessage = 'Ocorreu um erro, por favor tente mais tarde.';
       }
       setLoading(false);
       setError(systemErrorMessage);
+    }
+  };
+
+  // logout - sign out
+
+  const logout = () => {
+    checkIfIsCancelled();
+    signOut(auth);
+  };
+
+  //login
+
+  const login = async (data) => {
+    checkIfIsCancelled();
+    setLoading(true);
+    setError(false);
+
+    try {
+
+      await signInWithEmailAndPassword(auth, data.email, data.password);
+      setLoading(false);
+    } catch (error) {
+
+      let systemErrorMessage
+
+      if (error.message.includes('user-not-found')) {
+        systemErrorMessage = 'Usuário não encontrado.';
+      } else if (error.message.includes('wrong-password')) {
+        systemErrorMessage = 'Senha incorreta.';
+      } else {
+        systemErrorMessage = 'Ocorreu um erro, por favor tente mais tarde.';
+      }
+
+      setError(systemErrorMessage);
+      setLoading(false);
     }
 
   };
@@ -70,6 +105,8 @@ export const useAuthentication = () => {
     auth,
     createUser,
     error,
-    loading
+    loading,
+    logout,
+    login
   }
 };
