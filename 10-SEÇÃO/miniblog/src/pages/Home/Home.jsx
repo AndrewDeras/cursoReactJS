f41f262c14d4aa1a03cfe99f0489769e253dac1d
andrewDeras <andrewderas7@gmail.com>
2023-03-12 17:01:49 -0300

    mostrando posts na home page

--- 10-SEÇÃO/miniblog/src/pages/Home/Home.jsx
+++ 10-SEÇÃO/miniblog/src/pages/Home/Home.jsx
@@ -1,9 +1,43 @@
 //CSS
 import styles from './Home.module.css';
 
+// hooks
+import { useNavigate, Link } from 'react-router-dom';
+import { useState } from 'react';
+import { useFetchDocuments } from '../../hooks/useFetchDocuments';
+
+//components
+import PostDetail from '../../components/PostDetail';
+
 const Home = () => {
+  const [query, setQuery] = useState('');
+  const { documents: posts, loading } = useFetchDocuments('posts');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div>Home</div>
+    <div className={styles.home}  >
+      <h1>Vejas nossos posts mais recentes</h1>
+      <form className={styles.search_form} onSubmit={handleSubmit} >
+        <input
+          type="text"
+          placeholder='Ou busque por tags...'
+          onChange={({ target }) => setQuery(target.value)} />
+        <button className="btn btn-dark">Pesquisar</button>
+      </form>
+      <div>
+        {loading && <p>Carregando...</p>}
+        {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
+        {posts && posts.length === 0 && (
+          <div className={styles.nopost} >
+            <p>Não foram encontrados posts.</p>
+            <Link className='btn' to={'/posts/create'} >Criar primeiro post.</Link>
+          </div>
+        )}
+      </div>
+    </div>
   )
 }
 
