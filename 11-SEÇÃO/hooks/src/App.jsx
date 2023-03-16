1f9431987e799c1530589cb4f27de43a742e8bb3
andrewDeras <andrewderas7@gmail.com>
2023-03-15 21:57:40 -0300

    Aprendendo mais sobre o useContext

--- 11-SEÇÃO/hooks/src/App.jsx
+++ 11-SEÇÃO/hooks/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 //pages
 import Home from "./pages/Home"
 import About from "./pages/About";
+//hook
+import { HookUseContext } from "./components/HookUseContext";
 
 
 function App() {
@@ -9,20 +11,22 @@ function App() {
   return (
     <div >
       <h1>React Hooks</h1>
-      <BrowserRouter>
-        <ul>
-          <li>
-            <Link to={'/'} >Home</Link>
-          </li>
-          <li>
-            <Link to={'/about'} >About</Link>
-          </li>
-        </ul>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </BrowserRouter>
+      <HookUseContext>
+        <BrowserRouter>
+          <ul>
+            <li>
+              <Link to={'/'} >Home</Link>
+            </li>
+            <li>
+              <Link to={'/about'} >About</Link>
+            </li>
+          </ul>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </BrowserRouter>
+      </HookUseContext>
     </div>
   )
 }
